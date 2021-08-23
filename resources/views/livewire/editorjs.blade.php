ce7bae52386d3deb92715f703e6c39e04809940b
Mission10 <78219928+mission10hq@users.noreply.github.com>
2021-08-23 12:19:57 +0100

    Update editorjs.blade.php

--- resources/views/livewire/editorjs.blade.php
+++ resources/views/livewire/editorjs.blade.php
@@ -1,6 +1,6 @@
 <div
     x-data="editorInstance('data', '{{ $editorId }}', {{ $readOnly ? 'true' : 'false' }}, '{{ $placeholder }}', '{{ $logLevel }}')"
-    x-init="init()"
+   
     class="{{ $class }}"
     style="{{ $style }}"
     wire:ignore
